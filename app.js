e15d18a6a7986bc22de29e99df610cdfd962b554
keighty <k80.leonard@gmail.com>
2014-10-11 09:57:38 -0700

    Partition elements into rows and style using bootstrap.

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var playbills = angular.module("playbillApp", []);
 
 function PlaybillController($scope, $http) {
-  $scope.playbills = function() {
+  (function getPlaybills() {
     var playbillUrl = 'http://localhost:3030/playbills';
     $http.get(playbillUrl).success(function(data) {
       $scope.playbills = data;
+      $scope.playbillRows = partition(data, 3);
     }).error(function(response) {
       console.log(response.message);
     });
-  };
+  })();
 
-  $scope.playbills();
+  function partition(input, size) {
+    var rows = [];
+    for (var i=0; i < input.length; i+=size) {
+      rows.push(input.slice(i, i+size));
+    }
+    return rows;
+  }
 }

c43a3046d8367c23d1e11ac9f451c79de4e72409
keighty <k80.leonard@gmail.com>
2014-10-20 08:08:44 -0700

    Feature parity after directory restructuring.

--- static/app.js
+++ static/app.js
@@ -1,26 +1,29 @@
 var playbills = angular.module("playbillApp", []);
 
-function PlaybillController($scope, $http) {
-  (function getPlaybills() {
-    var playbillUrl = 'http://localhost:3030/playbills';
-    $http.get(playbillUrl).success(function(data) {
-      $scope.playbills = data;
-      $scope.playbillRows = partition(data, 3);
-    }).error(function(response) {
-      console.log(response.message);
-    });
-  })();
+playbills.controller('PlaybillController', ['$scope', '$http',
+  function($scope, $http) {
+    (function getPlaybills() {
+      var playbillUrl = 'http://localhost:3030/playbills';
+      $http.get(playbillUrl).success(function(data) {
+        $scope.playbills = data;
+        $scope.playbillRows = partition(data, 3);
+      }).error(function(response) {
+        console.log(response);
+      });
+    })();
 
-  function partition(input, size) {
-    var rows = [];
-    for (var i=0; i < input.length; i+=size) {
-      rows.push(input.slice(i, i+size));
+    function partition(input, size) {
+      var rows = [];
+      for (var i=0; i < input.length; i+=size) {
+        rows.push(input.slice(i, i+size));
+      }
+      return rows;
     }
-    return rows;
   }
-}
+]);
 
-playbills.controller('NewPlaybillController', ['$scope', '$http', function($scope, $http) {
+playbills.controller('NewPlaybillController', ['$scope', '$http',
+  function($scope, $http) {
     $scope.addPlaybill = function(show) {
       var addPlaybillUrl = 'http://localhost:3030/new_post';
       $http.post(addPlaybillUrl, show)

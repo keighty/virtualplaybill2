f7b8e3002bad206c69bb1dcad4950d6244adb922
keighty <k80.leonard@gmail.com>
2014-10-21 08:02:29 -0700

    Clean up routes file.

--- routes.js
+++ routes.js
@@ -14,20 +14,14 @@ module.exports = function(app) {
   app.post('/new_post', function(req, res, next) {
     var postAttributes = req.body;
     var post = _.extend(_.pick(postAttributes, 'url', 'title', 'company', 'author', 'music', 'showDate', 'image'), {
-        // userId: user._id,
-        // postAuthor: user.username,
         submitted: new Date().getTime(),
         commentsCount: 0
       });
 
-
     var newPost = new Post(post);
     newPost.save(function(err, doc){
-      if(err) {
-        res.send(err);
-      } else {
-        res.send(doc);
-      }
+      if(err) { res.send(err); }
+      else { res.send(doc); }
     });
   });
-}
+};

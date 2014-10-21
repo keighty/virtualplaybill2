de7b6095e4304dcca81ac707410a935c845d3523
keighty <k80.leonard@gmail.com>
2014-10-21 08:00:13 -0700

    Extract routes into new file.

--- server.js
+++ server.js
@@ -11,36 +11,5 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/playbills', function(req, res) {
-  Post.find()
-      .sort("-submitted")
-      .exec(function(err, doc) {
-        res.send(doc);
-      });
-});
-
-app.get('/add_post', function(req, res) {
-  res.send("hi from the server");
-});
-
-app.post('/new_post', function(req, res, next) {
-  var postAttributes = req.body;
-  var post = _.extend(_.pick(postAttributes, 'url', 'title', 'company', 'author', 'music', 'showDate', 'image'), {
-      // userId: user._id,
-      // postAuthor: user.username,
-      submitted: new Date().getTime(),
-      commentsCount: 0
-    });
-
-
-  var newPost = new Post(post);
-  newPost.save(function(err, doc){
-    if(err) {
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  });
-});
-
+require('./routes.js')(app);
 app.listen(3030);

659b7d6d23df4f081a29e2f593bd86071ada7933
keighty <k80.leonard@gmail.com>
2014-10-21 08:21:18 -0700

    Remove spurious quotes from Post model.

--- models/posts_model.js
+++ models/posts_model.js
@@ -1,18 +1,18 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-  "url" : String,
-  "title" : String,
-  "company" : String,
-  "author" : String,
-  "music" : String,
-  "choreographer" : String,
-  "showDate" : String,
-  "image": String,
-  "userId" : String,
-  "postAuthor" : String,
-  "submitted" : Number,
-  "commentsCount" : Number
+  url : String,
+  title : String,
+  company : String,
+  author : String,
+  music : String,
+  choreographer : String,
+  showDate : String,
+  image: String,
+  userId : String,
+  postAuthor : String,
+  submitted : Number,
+  commentsCount : Number
 });
 
 var Post = mongoose.model('Post', postSchema, 'posts');

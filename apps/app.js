51d9a179d256f170f240ffdfbf20790a83d63ccf
keighty <k80.leonard@gmail.com>
2015-03-21 13:57:57 -0700

    Extract routes.

--- apps/app.js
+++ apps/app.js
@@ -5,42 +5,6 @@ playbills.run(function($rootScope) {
   $rootScope.show = {};
 });
 
-playbills.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
-    $routeProvider.
-      when('/post/:postId', {
-        templateUrl: '/views/post.html',
-        controller: 'PostController'
-      }).
-      when('/add_post', {
-        templateUrl: '/views/post_form.html',
-        controller: 'PostController'
-      }).
-      when('/signin', {
-        templateUrl: '/views/signin.html',
-        controller: 'UserController'
-      }).
-      when('/signup', {
-        templateUrl: '/views/signup.html',
-        controller: 'UserController'
-      }).
-      when('/logout', {
-        templateUrl: '/views/signin.html',
-        controller: function() {
-          window.location.reload();
-        }
-      }).
-      when('/', {
-        templateUrl: 'views/index.html',
-        controller: 'PlaybillController'
-      });
-  }
-]);
-
 playbills.controller('PlaybillController', ['$scope', '$http', '$location',
   function($scope, $http, $location) {
     $http.get('/playbills').success(function(data) {
---
+++ apps/routes.js
@@ -0,0 +1,37 @@
+var app = angular.module("playbillApp");
+
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    $routeProvider.
+      when('/post/:postId', {
+        templateUrl: '/views/post.html',
+        controller: 'PostController'
+      }).
+      when('/add_post', {
+        templateUrl: '/views/post_form.html',
+        controller: 'PostController'
+      }).
+      when('/signin', {
+        templateUrl: '/views/signin.html',
+        controller: 'UserController'
+      }).
+      when('/signup', {
+        templateUrl: '/views/signup.html',
+        controller: 'UserController'
+      }).
+      when('/logout', {
+        templateUrl: '/views/signin.html',
+        controller: function() {
+          window.location.reload();
+        }
+      }).
+      when('/', {
+        templateUrl: 'views/index.html',
+        controller: 'PlaybillController'
+      });
+  }
+]);

8baf618f86d951ea77a3417144ab06399536e111
keighty <k80.leonard@gmail.com>
2015-03-28 09:27:57 -0700

    Extract cast methods into castform directive.

--- apps/cast.js
+++ apps/cast.js
@@ -16,19 +16,15 @@ app.directive("castform", function() {
   return {
     restrict: "E",
     templateUrl: "/views/cast_form.html",
-    controller: "CastController"
+    link: function($scope, element, attrs) {
+      $scope.addNewActor = function() {
+        var itemNo = $scope.show.cast.length;
+        $scope.show.cast.push({'name': '' , 'index': itemNo});
+      };
+
+      $scope.removeActor = function(index) {
+        $scope.show.cast.splice(index, 1);
+      };
+    }
   };
 });
-
-app.controller('CastController', ['$scope',
-  function($scope){
-    $scope.addNewActor = function() {
-      var itemNo = $scope.show.cast.length;
-      $scope.show.cast.push({'name': '' , 'index': itemNo});
-    };
-
-    $scope.removeActor = function(index) {
-      $scope.show.cast.splice(index, 1);
-    };
-  }
-]);

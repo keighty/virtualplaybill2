1466630821528845161490a8f391c85593a4e0bd
keighty <k80.leonard@gmail.com>
2015-04-02 20:39:13 -0700

    Remove user controller and use UserService to initialize app.

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,10 @@
 var app = angular.module("playbillApp", ['ngRoute', 'ui.bootstrap']);
 
-app.run(function($rootScope) {
-  $rootScope.user = {};
+app.run(function($rootScope, UserService) {
+  UserService.current_user().then(function(data) {
+    $rootScope.user = data;
+  }).catch(function(err) {
+    $rootScope.error = err;
+  });
   $rootScope.show = {};
 });

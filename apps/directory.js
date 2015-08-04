2bedbcfa9fff7147a0913d71295999634d4097bb
keighty <k80.leonard@gmail.com>
2015-08-03 21:58:12 -0700

    First draft of directory page.

--- apps/directory.js
+++ apps/directory.js
@@ -8,9 +8,35 @@ app.directive("directory", function() {
   };
 });
 
-app.controller('DirectoryController', ['$rootScope', '$scope', '$http', '$routeParams',
-  function($rootScope, $scope, $http, $routeParams){
-    console.log("hello directory");
-  }
+app.controller('DirectoryController', ['$scope', '$filter','PlaybillsService',
+  function($scope, $filter, PlaybillsService){
+    PlaybillsService.list().then(function(data) {
+      var sortedPlaybills = $filter('orderBy')(data, "title");
+      $scope.glossary = glossary(sortedPlaybills);
+    });
 
+    var glossary = function glossary(data) {
+      var collection = {};
+
+      data.forEach(function (item) {
+        var firstChar = item.title.charAt(0).toUpperCase();
+        if(collection[firstChar]) {
+          collection[firstChar].push(item.title);
+        } else {
+          collection[firstChar] = [item.title];
+        }
+      });
+
+      collection["#"] = [];
+      for (var key in collection) {
+        if (key.match(/\d/)) {
+          collection["#"] = collection['#'].concat(collection[key]);
+          delete collection[key];
+        }
+      }
+
+      return collection;
+    };
+
+  }
 ]);

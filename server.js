b0c554b81c0efece7b55ba6d1cc5d03986827313
keighty <k80.leonard@gmail.com>
2015-10-27 08:07:59 -0700

    Update mongo connect packages and modify expressSession.
    WIP
    
    WIP
    
    Upgrade mongo connect packages and replace expressSession object.

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
 /************
 ** Libraries
 *************/
-require('newrelic');
 var express = require('express');
 var expressSession = require('express-session');
-
 var http = require('http');
 var path = require('path');
-
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
-
-var mongoose = require('mongoose');
 var uriUtil = require('mongodb-uri');
+var uuid = require('node-uuid');
 
 /*********************
 ** Database connection
 *********************/
-var mongoStore = require('connect-mongo')({session: expressSession});
+var mongoose = require('mongoose');
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/virtual_playbill';
+var mongooseUri = uriUtil.formatMongoose(mongoUri);
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
-
-console.info("MongoURI", mongoUri);
-var mongooseUri = uriUtil.formatMongoose(mongoUri);
 
 mongoose.connect(mongooseUri, options);
 var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'connection error:'));
+
+var mongoStore = require('connect-mongo')({session: expressSession});
 
 /********
 ** Models
@@ -49,17 +45,17 @@ app.set('view engine', 'html');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(expressSession({
-  secret: 'SECRET',
-  cookie: {maxAge: 60*60*1000},
-  store: new mongoStore({
-    db: mongoose.connection.db,
-    collection: 'sessions'
-  }),
+
+var expressSession = expressSession({
+  genid: function(req) {
+    return uuid.v1()
+  },
+  secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true
-}));
+})
 
+app.use(expressSession);
 app.use('/apps', express.static('./apps'));
 app.use('/views', express.static('./views'));
 app.use('/static', express.static('./static'));
@@ -74,8 +70,10 @@ require('./routes.js')(app);
 /************
 ** App server
 *************/
+
 conn.once('open', function() {
   var port = process.env.PORT || 3030;
   app.listen(port);
+  console.info("MongoURI", mongoUri);
   console.log('Express server listening on port ' + port);
 });

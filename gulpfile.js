e55abe322ff1122adfc757b70c400b4405baa3cc
keighty <k80.leonard@gmail.com>
2015-11-17 08:23:43 -0800

    First browserified bundle.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
-var gulp = require('gulp')
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
 
-gulp.task('default', function () {
-  // code here
-  console.log("hello gulp")
-})
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './apps/app.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        // .pipe(uglify())
+        .on('error', gutil.log)
+    // .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});

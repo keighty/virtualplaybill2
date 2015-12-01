9541b3eabb8ecc405fa212c1204c40ecb8b206a6
keighty <k80.leonard@gmail.com>
2015-11-30 22:50:07 -0800

    Add company directory.

--- apps/controller/directory-controller.js
+++ apps/controller/directory-controller.js
@@ -1,17 +1,30 @@
-var DirectoryController = function ($scope, $filter, PlaybillsService){
+var DirectoryController = function ($scope, $route, $filter, PlaybillsService){
+  var sortedPlaybills
+  $scope.ordering = 'title'
+
+  $scope.$watch('ordering', function(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      $scope.glossary = glossary(sortedPlaybills, newVal)
+    }
+  })
+
   PlaybillsService.list().then(function (data) {
-    var sortedPlaybills = $filter('orderBy')(data, "title")
-    $scope.glossary = glossary(sortedPlaybills)
+    sortedPlaybills = $filter('orderBy')(data, $scope.ordering)
+    $scope.glossary = glossary(sortedPlaybills, $scope.ordering)
   })
 }
 
-function glossary (data) {
+function glossary (data, sortBy) {
+  CollectionBuilder.initializeCollection(sortBy)
   data.forEach(function (item) { CollectionBuilder.add(item) })
   return CollectionBuilder.collection
 }
 
 CollectionBuilder = {
-  collection: {'#': []},
+  initializeCollection: function (sortBy) {
+    this.collection = {}
+    this.sortBy = sortBy
+  },
   add: function (item) {
     if (this.isDigit(item)) { this.handleDigit(item) }
     else { this.handleString(item) }
@@ -20,18 +33,24 @@ CollectionBuilder = {
     return this.firstChar(item).match(/\d/)
   },
   firstChar: function (item) {
-    return item.title && item.title.charAt(0).toUpperCase() || 'unknown'
+    return item[this.sortBy] && item[this.sortBy].charAt(0).toUpperCase() || 'unknown'
   },
   handleDigit: function (item) {
-    this.collection['#'].push(item)
+    if (this.collection['#']) this.collection['#'].push(item)
+    else this.collection['#'] = [item]
   },
   handleString: function (item) {
-    var temp = this.firstChar(item)
+    var temp
+    if (this.sortBy === 'title') {
+      temp = this.firstChar(item)
+    } else {
+      temp = item[this.sortBy]
+    }
     if (this.collection[temp]) { this.collection[temp].push(item) }
     else { this.collection[temp] = [item] }
   }
 }
 
-DirectoryController.$inject = ['$scope', '$filter','PlaybillsService']
+DirectoryController.$inject = ['$scope', '$route', '$filter','PlaybillsService']
 
 module.exports = DirectoryController

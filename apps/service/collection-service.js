26084e41f806218a80cb6ede990fd529315f04d8
keighty <k80.leonard@gmail.com>
2017-06-05 18:37:57 -0700

    Extract collection service and simplify logic.

---
+++ apps/service/collection-service.js
@@ -0,0 +1,43 @@
+var CollectionService = function () {
+  var collection
+
+  return {
+    group: function (collection) {
+      this.collection = collection
+      return this
+    },
+    by: function (category) {
+      return orderByTitle(this.collection)
+    }
+  }
+
+  function orderByTitle (collection) {
+    var newCollection =  collection.reduce(function (acc, item) {
+      var first = firstChar(item.title)
+      if (typeof(acc[first]) === 'undefined') {
+        acc[first] = []
+      }
+      acc[first].push(item)
+      return acc
+    }, {})
+
+    delete newCollection.undefined
+    return newCollection
+  }
+
+  function firstChar (str) {
+    if (!str) { return }
+    switch(true) {
+      case /^[0-9]/.test(str):
+        return '#'
+      case /^[Tt]he\s\s*/.test(str):
+        return firstChar(str.replace(/^[Tt]he\s+/, ''))
+      case /^[a-zA-Z]/.test(str):
+        return str.charAt(0).toUpperCase()
+      default:
+        return firstChar(str.substring(1))
+    }
+  }
+}
+
+module.exports = CollectionService
--- apps/service/index.js
+++ apps/service/index.js
@@ -2,3 +2,4 @@ var app = angular.module("playbillApp");
 
 app.factory('UserService', require('./user-service.js'))
 app.factory("PlaybillsService", require('./playbills-service.js'))
+app.factory("CollectionService", require('./collection-service.js'))

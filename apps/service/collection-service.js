15c78f62e28115442b3af159061f7e16e1f3da97
keighty <k80.leonard@gmail.com>
2017-06-06 08:06:42 -0700

    Extract collection into module and add tests.

--- apps/service/collection-service.js
+++ apps/service/collection-service.js
@@ -1,3 +1,5 @@
+var Collection = require('./collection')
+
 var CollectionService = function () {
   var collection
 
@@ -13,56 +15,10 @@ var CollectionService = function () {
         case 'company':
           return this.collection.orderByCompany()
         default:
-          return this.collection.orderByTitle()
+          return this.collection.orderByActor()
       }
     }
   }
-}
-
-var Collection = function (data) {
-  this.collection = data || {}
-}
-
-Collection.prototype.orderByTitle = function () {
-  var newCollection =  this.collection.reduce(function (acc, item) {
-    var first = firstChar(item.title)
-    if (typeof(acc[first]) === 'undefined') {
-      acc[first] = []
-    }
-    acc[first].push(item)
-    return acc
-  }, {})
-
-  delete newCollection.undefined
-  return newCollection
-
-  function firstChar (str) {
-    if (!str) { return }
-    switch(true) {
-      case /^[0-9]/.test(str):
-        return '#'
-      case /^[Tt]he\s\s*/.test(str):
-        return firstChar(str.replace(/^[Tt]he\s+/, ''))
-      case /^[a-zA-Z]/.test(str):
-        return str.charAt(0).toUpperCase()
-      default:
-        return firstChar(str.substring(1))
-    }
-  }
-}
-
-Collection.prototype.orderByCompany = function () {
-  var newCollection =  this.collection.reduce(function (acc, item) {
-    var first = item.company
-    if (typeof(acc[first]) === 'undefined') {
-      acc[first] = []
-    }
-    acc[first].push(item)
-    return acc
-  }, {})
-
-  delete newCollection.undefined
-  return newCollection
 }
 
 module.exports = CollectionService
